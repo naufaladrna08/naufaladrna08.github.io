4bada1e354d07afc25d242436ceb0da1baf16629
naufaladrna08 <naufaladrn90@gmail.com>
2021-12-26 01:02:41 +0700

    fixed responsive design

--- inos-docs/index.php
+++ inos-docs/index.php
@@ -17,6 +17,9 @@ if (isset($_GET['p'])) {
 
 <html>
 <head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title> InOS Documentation </title>
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
